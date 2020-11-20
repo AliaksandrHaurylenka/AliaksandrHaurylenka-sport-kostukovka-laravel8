37acc02f5a07031c0120d6754483da9efded264e
AliaksandrHaurylenka <goric0312@mail.ru>
2020-11-20 07:18:57 +0300

    asd

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix');
 
 mix
   .setPublicPath('public/build')
-  .setResourceRoot('build')
+  .setResourceRoot('/build/')
   .js('resources/assets/js/app.js', 'js')
   .sass('resources/assets/sass/app.scss', 'css')
   .sass('resources/assets/sass/my_style.scss', 'css')
