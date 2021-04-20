91d8ac0c5cbac70aad2c3417d0c8de4d32c184e6
Aliaksandr <goric0312@mail.ru>
2021-04-20 15:59:17 +0300

    asd

--- resources/views/site/blocks/sections_menu.blade.php
+++ resources/views/site/blocks/sections_menu.blade.php
@@ -6,10 +6,31 @@
             aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
       <div class="d-none d-sm-block">
+				<div class="sections-icons d-flex justify-content-center">
+
+					@if(isset($sportSections))
+            @foreach($sportSections as $section)
+              <div class="">
+                <div class="view overlay sport_icons btn-light-blue" onclick="window.location='{{ route('section', [$section->id, $section->slug]) }}'">
+                  <div class="sport_icons_border">
+                    <div class="sport_icons_bg btn-light-blue">
+                      <img class="" src="/images/sections/{{$section->photo_section_menu}}" alt="{{ $section->title }}">
+                    </div>
+                  </div>
+                  
+                  <span class="mask rgba-white-slight" title="{{ $section->title }}"></span>
+                </div>
+              </div>
+            @endforeach
+          @endif
+
+				</div>
+
+				
         <!-- Card deck -->
-        <div class="card-group">
+        {{-- <div class="card-group">
           @if(isset($sportSections))
             @foreach($sportSections as $section)
               <!-- Card -->
@@ -28,15 +49,15 @@
                 <!--Card content-->
                 <div class="card-body d-none d-lg-block for-active-button text-center">
                   <!--Text-->
-                  {{-- <p class="card-text">{!! getLengthString($section->description, 50) !!}</p>
-                  <h2 class="card-text">{!! $section->title !!}</h2> --}}
+                  <p class="card-text">{!! getLengthString($section->description, 50) !!}</p>
+                  <h2 class="card-text">{!! $section->title !!}</h2>
                   <a href="{{ route('section', [$section->id, $section->slug]) }}" class="btn btn-light-blue btn-md text-white">{!! $section->title !!}</a>
                 </div>
               </div>
               <!-- Card -->
             @endforeach
           @endif
-        </div>
+        </div> --}}
       </div>
       <!-- Card deck -->
       <!-- Links -->
